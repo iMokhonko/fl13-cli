24db5f213e268533a257ee997637209d1843e108
Ivan Mokhonko <ivan.mokhonko@onereach.com>
2023-09-11 23:14:56 +0300

    chore: change

--- src/commands/deploy/index.js
+++ src/commands/deploy/index.js
@@ -1,6 +1,8 @@
 
-const { spawn } = require('child_process');
+const { spawn, exec } = require('child_process');
 const fs = require('fs');
+
+const _get = require('lodash/get');
 
 const runCommand = async ({ cmd, args, cwd, shell = false }) => {
 	return new Promise(resolve => {
@@ -35,39 +37,150 @@ const readJsonFile = (path) => {
   }
 };
 
+const replaceTfVarsPlaceholderWithValues = 
+  (str, replacements) => 
+    str.replaceAll(
+      /\<tf-outputs:(.*?)\>/g, 
+      (matchedString, varName) => _get(replacements, varName, matchedString)
+    )
+
 const handler = async (argv) => {  
-  const { 
-    serviceName, 
-    domainName 
-  } = readJsonFile('./terraform/project.json') ?? {};
-
-  const commands = [
-    { cmd: 'terraform', args: ['init', '-reconfigure'], cwd: `./terraform/init` }, // init terraform
-    { cmd: 'terraform', args: ['workspace', 'select', '-or-create', `${argv.env}.${serviceName}`], cwd: `./terraform/init` }, // select/create workspace
-    { cmd: 'terraform', args: ['apply', `--var`, `domain_name=${domainName}`, `--var`, `service_name=${serviceName}`, `--var`, `env=${argv.env}`, '--auto-approve'], cwd: `./terraform/init` },
-
-    { cmd: 'terraform', args: ['init', '-reconfigure'], cwd: `./terraform/deploy` }, // init terraform
-    { cmd: 'terraform', args: ['workspace', 'select', '-or-create', `${argv.feature}.${argv.env}.${serviceName}`], cwd: `./terraform/deploy` }, // select/create workspace
-    { cmd: 'terraform', args: ['apply', `--var`, `domain_name=${domainName}`, `--var`, `service_name=${serviceName}`, `--var`, `env=${argv.env}`, `--var`, `feature=${argv.feature}`, '--auto-approve'], cwd: `./terraform/deploy` },
-    { cmd: 'terraform', args: ['output', '-json', '>', 'output.json'], cwd: `./terraform/deploy`, shell: true },
-  ];
-
-  await runCommands(commands);
+  const {
+    env = 'dev',
+    feature = 'master'
+  } = argv;
 
   const {
-    s3_bucket_name,
-    cloudfront_distribtuion_id
-  } = readJsonFile('./terraform/deploy/output.json') ?? {};
+    terraformResources = [],
+    deployChain = []
+  } = readJsonFile(`./terraform/${env}/service.json`) ?? {};
 
-  await runCommands([
-    { cmd: 'npm', args: ['run', 'build'] },
-    { cmd: 'aws', args: ['s3', 'sync', './dist', `s3://${s3_bucket_name.value}`, '--profile', 'default'] },
-    { cmd: 'aws', args: ['cloudfront', 'create-invalidation', '--distribution-id', cloudfront_distribtuion_id.value, '--paths', '/*', '--profile', 'default'] }
-  ]);
+  // object for storing outputs from terraform
+  const tfOutputs = {};
 
-  await runCommands([
-    { cmd: 'rm', args: ['output.json'], cwd: `./terraform/deploy`, shell: true },
-  ]);
+  while(terraformResources.length) {
+    const {
+      folderName, // tf directory path
+      outputName,
+      variables = {},
+      global
+    } = terraformResources.shift();
+
+    // terraform resources directory
+    const cwd = `./terraform/${env}/${folderName}`
+
+    const commands = [
+      // init terraform when running this command
+      { 
+        cmd: 'terraform',
+         args: ['init', '-reconfigure'], 
+         cwd
+      },
+    ];
+
+    // if resources are not marked as global create terraform workspace
+    if(!global && feature !== 'master') {
+      commands.push(
+        { 
+          cmd: 'terraform', 
+          args: ['workspace', 'select', '-or-create', feature], 
+          cwd 
+        },
+      )
+    }
+
+    const varsArray = Object.entries(variables);
+    const argsWithReplacesVars = varsArray.reduce((replacedVars, [varName, varValue]) => {
+      return [
+        ...replacedVars,
+        [varName, replaceTfVarsPlaceholderWithValues(varValue, tfOutputs)]
+      ];
+    }, []);
+
+    const argsWithReplacesCmdLineArgs = argsWithReplacesVars.map(([varName, varValue]) => [
+      varName,
+      argv[varName] ? argv[varName] : varValue
+    ]);
+
+    const stringVars = argsWithReplacesCmdLineArgs.reduce(
+      (memo, [varName, varValue]) => 
+      [...memo, '--var', `${varName}=${varValue}`],
+      []
+    );
+
+    if(feature && !global) {
+      stringVars.push('--var', `feature=${feature}`)
+    }
+
+    if(env) {
+      stringVars.push('--var', `env=${env}`)
+    }
+
+    commands.push(
+      {
+        cmd: 'terraform',
+        args: [
+          'apply', 
+          ...stringVars,
+          '--auto-approve'
+        ],
+        cwd
+      },
+      { 
+        cmd: 'terraform', 
+        args: ['output', '-json', '>', 'output.json'], 
+        cwd, 
+        shell: true 
+      }
+    );
+
+    await runCommands(commands);
+
+    const outputs = readJsonFile(`${cwd}/output.json`) ?? {};
+    
+    tfOutputs[outputName] = Object.entries(outputs).reduce((memo, [outputName, { value }]) => ({
+      ...memo,
+      [outputName]: value
+    }), {});
+
+    await runCommands(
+      [
+        {
+          cmd: 'rm',
+          args: ['output.json'],
+          cwd, 
+          shell: true
+        },
+        { 
+          cmd: 'terraform', 
+          args: ['workspace', 'select', '-or-create', 'default'], 
+          cwd 
+        },
+      ]
+    );
+
+    console.log(outputName + " ->", tfOutputs);
+  }
+
+  const execCommandsString = deployChain.reduce((allCommands, command, index) => {
+    if(index === 0) {
+      return replaceTfVarsPlaceholderWithValues(command, tfOutputs);
+    } else {
+      return `${allCommands} && ${replaceTfVarsPlaceholderWithValues(command, tfOutputs)}`
+    }
+  }, '');
+
+  exec(execCommandsString, (error, stdout, stderr) => {
+    if (error) {
+        // You can print the error if there is one
+        console.error(error);
+        return;
+     }
+     // stdout gives you the output from your command
+     console.log(stdout);
+     // stderr gives you the error (if any) from your command
+     console.log(stderr);
+  });
 };
 
 const args = {
